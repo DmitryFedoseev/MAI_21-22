121c5497c9d950b81b699e2340fb01c05fa671c0
DmitryFedoseev <71728626+DmitryFedoseev@users.noreply.github.com>
2021-11-29 05:44:05 +0300

    Add files via upload

---
+++ Seminars/pz-4 STL/stack.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <sstream>
+#include <stack>
+using namespace std;
+
+int main() {
+	cout << "Enter the number of nodes:\n";
+	int N = 0;
+	cin >> N;
+	vector <vector<int>> Nodes(N);
+	string stringValue;
+	getline(cin, stringValue);
+	for (int i = 0; i < N; i++) {
+		getline(cin, stringValue);
+		for (int j = 0; j < N; j++) {
+			Nodes[i].push_back(0);
+		}
+		istringstream stringNum(stringValue);
+		int connectNodes;
+
+		while (stringNum >> connectNodes) {
+			if (connectNodes != 0) {
+				Nodes[i][connectNodes - 1] = 1;
+			}
+		}
+	}
+	cout << endl;
+	cout << "Link table\n" << endl;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << Nodes[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	vector <vector<int>> result(N);
+	stack <int> x;
+	for (int i = 0; i < N; i++) {
+		set <int> resultOneNode = {};
+
+		for (int j = 0; j < N; j++)
+		{
+			result[i].push_back(0);
+		}
+
+		x.push(i);
+		while (!x.empty()) {
+			int temp = x.top();
+			x.pop();
+			resultOneNode.insert(temp);
+			for (int j = 0; j < N; j++) {
+				if (Nodes[temp][j] == 1 && resultOneNode.find(j) == resultOneNode.end()) {
+					x.push(j);
+				}
+			}
+		}
+		for (auto it = resultOneNode.begin(); it != resultOneNode.end(); it++)
+		{
+			result[i][*it] = 1;
+		}
+	}
+
+	cout << "Reachability matrix\n" << endl;
+	for (auto el : result)
+	{
+		for (auto em : el)
+		{
+			cout << em << " ";
+		}
+		cout << endl;
+	}
+}
